26688532ff4eec5d6a4e27f54b38af5e262531e3
LuisZamMX <luis.felipe.zamora@comunidad.unam.mx>
2019-01-21 23:49:04 -0600

    Se agregaron los archivos con el index terminado y las plantillas

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//nav
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+//Slider
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+
+//parallax
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
